728b90d33486979b0bd46ee59fe57b0a528cb944
thanhyou00 <truc06082000@gmail.com>
2022-05-05 22:34:16 +0700

    Cài đặt và cấu hình Prettier

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,21 @@
 // Layouts
-import { HeaderOnly } from "~/components/Layout";
+import { HeaderOnly } from '~/components/Layout';
 
 // Pages
-import Home from "~/pages/Home";
-import Following from "~/pages/Following";
-import Profile from "~/pages/Profile";
-import Upload from "~/pages/Upload";
-import Search from "~/pages/Search";
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
 
 // Routes public
 
 const publicRoutes = [
-  { path: "/", component: Home },
-  { path: "/following", component: Following },
-  { path: "/profile", component: Profile },
-  { path: "/upload", component: Upload, layout : HeaderOnly },
-  { path: "/search", component: Search, layout : null },
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
 ];
 
 const privateRoutes = [];
