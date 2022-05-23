e3b5bce1d2acee90e8a812e156ffb0361f620070
thanhyou00 <truc06082000@gmail.com>
2022-05-23 23:54:06 +0700

    74. Xây dựng Logic phần Header #3

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -7,6 +7,7 @@ import 'tippy.js/dist/tippy.css';
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
 import { Wrapper as PoperWrapper } from '~/components/Popper';
+import { useDebounce } from '~/hooks';
 import styles from './Search.module.scss';
 
 const cx = classNames.bind(styles);
@@ -17,17 +18,19 @@ function Search() {
     const [showResult, setShowResult] = useState(true);
     const [loading, setLoading] = useState(false);
 
+    const debounced = useDebounce(searchValue, 500);
+
     const inputRef = useRef();
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([]);
             return;
         }
 
         setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then((res) => res.json())
             .then((res) => {
                 setSearchResult(res.data);
@@ -36,7 +39,7 @@ function Search() {
             .catch(() => {
                 setLoading(false);
             });
-    }, [searchValue]);
+    }, [debounced]);
     const handleClear = () => {
         setSearchValue('');
         setSearchResult([]);
---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+function useDebounce(value, delay) {
+    const [debounceValue, setDebounceValue] = useState(value);
+
+    useEffect(() => {
+        const handler = setTimeout(() => setDebounceValue(value), delay);
+
+        return () => clearTimeout(handler);
+    }, [value, delay]);
+    return debounceValue;
+}
+
+export default useDebounce;
