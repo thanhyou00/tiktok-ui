84d4bf169303796d366db5bb3a0cd4e9d5fafba4
trucnv <trucnv.dev@gmail.com>
2023-06-14 09:29:35 +0700

    update yt

--- src/App.js
+++ src/App.js
@@ -4,6 +4,35 @@ import { publicRoutes } from '~/routes/routes';
 import DefaultLayout from '~/layouts';
 
 function App() {
+    function updateTitle() {
+        var videoID = '8rTKxkS5Vss';
+
+        var part = 'snippet,statistics';
+        var params = { id: videoID };
+
+        var response = YouTube.Videos.list(part, params);
+        var video = response.items[0];
+        var videoViewsCount = video.statistics.viewCount;
+        var videoLikeCount = video.statistics.likeCount;
+        var videoDislikeCount = video.statistics.dislikeCount;
+        var videoCommentCount = video.statistics.commentCount;
+        var videoTitle =
+            'Stacks IT || This video has ' +
+            videoViewsCount +
+            ' views and ' +
+            videoLikeCount +
+            ' likes and ' +
+            videoDislikeCount +
+            ' dislikes and ' +
+            videoCommentCount +
+            ' comments !';
+
+        video.snippet.title = videoTitle;
+
+        try {
+            YouTube.Videos.update(video, part);
+        } catch (e) {}
+    }
     return (
         <Router>
             <div className="App">
